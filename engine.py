f090ee4715b6b5f9bc505b7bdafcd7f1b1d7462a
Rajiv Kushwaha <raj25by10@gmail.com>
2023-02-27 12:58:45 +0530

    Make Engine a singleton

--- engine.py
+++ engine.py
@@ -1,12 +1,17 @@
 from event import Event
 
-class Engine(object):
+class The(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(The, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Engine(object, metaclass=The):
     def __init__(self, policies):
         self.policies = policies
 
     def on_event(self, event: Event):
         for policy in self.policies:
             policy.run(event)
-
-
 
